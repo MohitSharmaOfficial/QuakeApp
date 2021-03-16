736d60ad770c3c9676806dd1b41513d0174559c3
Mohit Sharma <Vipersharma.88@gmail.com>
2021-03-16 12:50:54 +0530

    Initialize Repo

---
+++ app/src/main/java/com/example/quake_app/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.quake_app;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
+
+        ListView earthquakeListView = findViewById(R.id.list);
+
+        final EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
+        earthquakeListView.setAdapter(adapter);
+
+        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Earthquake currentEarthquake = adapter.getItem(i);
+
+                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                startActivity(websiteIntent);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/quake_app/NewsAdapter.java
@@ -0,0 +1,24 @@
+package com.example.quake_app;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class NewsAdapter extends ArrayAdapter<Earthquake> {
+
+    public NewsAdapter(@NonNull Context context, int resource, @NonNull List<Earthquake> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
